262a89769789460bb59cf79a0f99e2df2eed3209
jagan-c <65348083+jagan-c@users.noreply.github.com>
2020-05-31 02:31:22 +0530

    Updates

---
+++ precache-manifest.43cb14fc42130e0f4add0a2cf01a3c2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00b747702302fa410eb89b9653f8c8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd8109456e4b40f0791a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1b5caeedce81a69b6a88",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dd8109456e4b40f0791a",
+    "url": "/static/js/2.19c3806d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.19c3806d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5caeedce81a69b6a88",
+    "url": "/static/js/main.02e0a7cd.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "d3b1fd37f7aeea7a264a07691a2956ce",
+    "url": "/static/media/logo.d3b1fd37.svg"
+  }
+]);
